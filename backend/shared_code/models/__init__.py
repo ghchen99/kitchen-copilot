7d9fb4d739c48de96222ee7c892e6b32e7e32714
George Chen <ghchen10@outlook.com>
2026-07-03 10:57:40 +0100

    v2 init

--- backend/shared_code/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Models package initialization
-This makes the models importable directly from the models package
-"""
-
-from .ingredients import IngredientsResult
-from .recipes import Recipe, RecipeCollection
-
-__all__ = ['IngredientsResult', 'Recipe', 'RecipeCollection']
--- backend/shared_code/prompts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Prompts package initialization
-This makes the prompt functions importable directly from the prompts package
-"""
-
-from .recipe_prompt import get_recipe_system_prompt
-from .vision_prompt import get_vision_system_prompt
-
-__all__ = ['get_recipe_system_prompt', 'get_vision_system_prompt']
